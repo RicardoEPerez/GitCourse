70790a03e82e60ef597989d91d54814d7564c938
RicardoEPerez <ric.emm.p.c@gmail.com>
2021-04-09 17:04:06 -0500

    Con los comandos escritos en app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('hellow worl');
+//Nota de los comandos
+//git init para comenzar a capturar una carpeta
+    //Se puede llegar desde el git bash (cd "Carpeta") o seleccionando la carpeta
+//git add . Agrega todos los archivos
+//git  status muestra los cambios
+//git log muestra las claves de las versiones
+//git checkout es para cambiar entre V. del projecto y deshacer cambios en el
+//git branch es para hacer hilos de projectos
